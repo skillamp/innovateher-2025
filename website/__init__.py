e496c2c5b01a23a379819adccdff7202474a81e8
Amruta Killampalli <ksavarshini@gmail.com>
2025-01-25 12:17:14 -0500

    Changes - delete cache and other stuff

--- website/__init__.py
+++ website/__init__.py
@@ -12,6 +12,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    from .views import views
+    from .auth import auth
+
     return app
 
 
--- website/views.py
+++ website/views.py
@@ -6,3 +6,6 @@ import json
 
 views = Blueprint('views', __name__)
 
+@views.route('/')
+def home():
+    return "<h1>Test</h1>"
