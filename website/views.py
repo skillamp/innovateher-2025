352fbaae1a4820bd461e615b5bf9c9580c55757c
Amruta Killampalli <ksavarshini@gmail.com>
2025-01-25 12:34:56 -0500

    add auth and views code

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required, current_user
-from .models import Note
-from . import db
-import json
+#from flask_login import login_required, current_user
+#from .models import Note
+#from . import db
+#import json
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
